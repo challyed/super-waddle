fa24cf8848853fce226a458465b844d0f55eeeaa
Edwin Chalumeau <edwinchalumeau@outlook.com>
2022-10-15 20:09:29 -0400

    climbing rocks

---
+++ Personal Project/Assets/ClimbController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbController : MonoBehaviour
+{
+    public GameObject xrRig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
